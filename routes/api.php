0870855b281fa241f178670e9419f588bc3fea26
Gunther37 <jmelvin1994@gmail.com>
2025-02-13 09:20:57 -0600

    Commit test from WSL

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/***************************** TEST ****************************/
+/***************************************************************/
+
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
